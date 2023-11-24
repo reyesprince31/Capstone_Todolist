c5060beb2815ceb3846b98170f3fb9b4be14a301
Prince Reyes <krayzerprince@gmail.com>
2023-11-24 15:01:41 +0800

    work with post todo

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let todoList = []; // Use let instead of const to allow modification
+
+app.use(express.urlencoded({ extended: true })); // Use extended: true for nested objects
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { todoList }); // Pass todoList as data object
+});
+
+app.post("/submit", (req, res) => {
+  const { todoItem } = req.body;
+
+  const newTodo = {
+    id: Math.random(),
+    item: todoItem,
+  };
+
+  todoList.push(newTodo); // Update todoList array
+  console.log(todoList);
+
+  res.render("index.ejs", { todoList }); // Pass updated todoList
 });
 
 app.listen(PORT, () => {
